6e08fe2dfb89b906ae498b70164bff85c83e2b96
krazevedo <kaio.azevedo85@gmail.com>
2015-03-06 09:40:31 -0300

    atualizacoes

--- src/main/java/com/ciandt/selenium/helpers/DataDriven.java
+++ src/main/java/com/ciandt/selenium/helpers/DataDriven.java
@@ -16,7 +16,7 @@ public class DataDriven {
 		//Create Object of Properties Class.
 		obj = new Properties();   
 		//Create Object of FileInputStream Class. Pass file path.
-		File file = new File("C:\\Users\\kaior\\workspace\\Regressão\\src\\main\\resources\\regressao.properties");
+		File file = new File("src\\main\\resources\\regressao.properties");
 		FileInputStream fileInput = null;
 		fileInput = new FileInputStream(file);
 		//Pass object reference objfile to load method of Properties object.
